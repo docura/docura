9e9339c977406d6ce184dae93712a7131cccf31e
Scott Rothrock <scott@blackwhitedog.com>
2023-08-04 12:57:15 +0000

    Makes dropdowns automatically close as per #3

--- assets/js/component/dropdown.js
+++ assets/js/component/dropdown.js
@@ -1,6 +1,17 @@
 let dropdowns = document.querySelectorAll('.dropdown')
+const openSelector = '.dropdown-menu.show';
+
 dropdowns.forEach(el => {
     el.addEventListener('click', function () {
+        document.querySelectorAll(openSelector).forEach(openDropdownEl => openDropdownEl.classList.remove('show'));
         el.querySelector('.dropdown-menu').classList.toggle('show');
     })
+});
+
+document.body.addEventListener('click', function (e) {
+    let offsetParent = e.target.offsetParent;
+    let isDropdownMenu = (offsetParent) ? offsetParent.classList.contains("dropdown") : false;
+
+    if (!isDropdownMenu)
+        document.querySelectorAll(openSelector).forEach(el => el.classList.remove('show'));
 });
