277d11bc9ebb9342d5d42d41bb3af9b8866321ee
Scott Rothrock <scott@blackwhitedog.com>
2023-08-04 14:02:12 +0000

    Fixes bug where menu could not be closed by clicking the button

--- assets/js/component/dropdown.js
+++ assets/js/component/dropdown.js
@@ -2,15 +2,19 @@ let dropdowns = document.querySelectorAll('.dropdown')
 const openSelector = '.dropdown-menu.show';
 
 dropdowns.forEach(el => {
-    el.addEventListener('click', function () {
+    el.addEventListener('click', function (e) {
+        let alreadyShown = el.querySelector('.dropdown-menu.show');
+
         document.querySelectorAll(openSelector).forEach(openDropdownEl => openDropdownEl.classList.remove('show'));
-        el.querySelector('.dropdown-menu').classList.toggle('show');
+
+        if (!alreadyShown)
+            el.querySelector('.dropdown-menu').classList.toggle('show');
     })
 });
 
 document.body.addEventListener('click', function (e) {
     let offsetParent = e.target.offsetParent;
-    let isDropdownMenu = (offsetParent) ? offsetParent.classList.contains("dropdown") : false;
+    let isDropdownMenu = (offsetParent) ? offsetParent.classList.contains('dropdown') : false;
 
     if (!isDropdownMenu)
         document.querySelectorAll(openSelector).forEach(el => el.classList.remove('show'));
