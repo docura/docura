7cf0a2844d5882b047e086cde548249175aa9727
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2023-08-06 00:13:48 +0800

    🌱 Fix offcanvas sidebar & toc

--- assets/js/component/article-nav.js
+++ assets/js/component/article-nav.js
@@ -1,6 +1,7 @@
 const btnArticleNavMenu = document.querySelector("#article-nav-menu-btn")
 if (btnArticleNavMenu) {
     btnArticleNavMenu.addEventListener('click', function () {
+        document.body.classList.remove('offcanvas-toc-on');
         document.body.classList.add('offcanvas-sidebar-on')
     });
 }
@@ -8,6 +9,7 @@ if (btnArticleNavMenu) {
 const btnArticleNavToc = document.querySelector("#article-nav-toc-btn")
 if (btnArticleNavToc) {
     btnArticleNavToc.addEventListener('click', function () {
+        document.body.classList.remove('offcanvas-sidebar-on');
         document.body.classList.add('offcanvas-toc-on')
     });
 }
@@ -23,5 +25,12 @@ const btnCloseArticleNavToc = document.querySelector("#toc .btn-close")
 if (btnCloseArticleNavToc) {
     btnCloseArticleNavToc.addEventListener('click', function () {
         document.body.classList.remove('offcanvas-toc-on')
+    });
+
+    const tocLinks = document.querySelectorAll("#toc ul a");
+    tocLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            document.body.classList.remove('offcanvas-toc-on')
+        });
     });
 }
