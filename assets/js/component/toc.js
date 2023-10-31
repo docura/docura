0294d4949ba15779abb6d1bccea8c2269580b2fd
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2023-10-31 13:18:32 +0800

    🌱 Set the content as the default focus

--- assets/js/component/toc.js
+++ assets/js/component/toc.js
@@ -1,6 +1,10 @@
 const fromLargeTablet = window.matchMedia('(min-width: 1024px)');
 const tocSticky = document.querySelector("#toc .sticky")
 if (fromLargeTablet && tocSticky) {
+    if (document.referrer.includes(window.location.origin) && !window.location.hash) {
+        window.scrollTo(0, 81)
+    }
+
     window.addEventListener("scroll", function () {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             tocSticky.style.top = "20px";
